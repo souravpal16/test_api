7a8dd7602681e8cd21dc5def870ccafcd0483dca
souravpal16 <souravpal.4916@gmail.com>
2022-01-22 16:42:21 +0530

    test commits

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.send({"result": "Welcome to test api"});
+});
+
+app.get('/about', (req, res)=>{
+    res.send({"result":"Welcome to about endpoint"});
+});
+
+app.get('/user/:username', (req, res) => {
+    const username = req.params.username;
+    res.send({"result":`Welcome user: ${username}`});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server is up on port: ${PORT}`);
+})
